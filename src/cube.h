058d62cee583f1c75336e63813f0a51385faadea
Benjamin LRX <benjamin.l@hotmail.fr>
2016-05-18 23:57:18 +0200

    Implementation of display() (Not tested with the cube)

--- src/cube.h
+++ src/cube.h
@@ -3,8 +3,15 @@
 
 #include <stdint.h>
 
+#define BUFFER_MAX_INDEX 64
+
 typedef struct _cube {
     uint16_t led_buffer[10][10];
 } cube;
+
+
+cube *new_cube();
+
+void display(char * dev, cube *cube);
 
 #endif 
