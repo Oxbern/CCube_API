285483e0fb53affe411f3e9ba8c516385a04fbcd
Benjamin LRX <benjamin.l@hotmail.fr>
2016-05-18 22:22:26 +0200

    Fix cube.c bug

--- src/cube.c
+++ src/cube.c
@@ -1,14 +1,17 @@
 #include <stdint.h>
+#include <stdlib.h>
 
-cube new_cube() {
-    cube newCube = malloc(sizeof(cube));
+#include "cube.h"
+
+cube *new_cube() {
+    cube *newCube = malloc(sizeof(cube));
 
     for (int i = 0; i <= 10; ++i) {
 	for (int j = 0; j < 10; ++j)
-	    newCube.led_buffer[i][j] = 0b0000000000;
-	newCube.led_buffer[i][10] = 1 << i;
+	    newCube->led_buffer[i][j] = 0b0000000000;
+	newCube->led_buffer[i][10] = 1 << i;
     }
-    newCube.led_buffer[10][10] = 0b0000000000;
+    newCube->led_buffer[10][10] = 0b0000000000;
 
     return newCube;
 }
@@ -17,6 +20,4 @@ void display() {
     
 }
 
-void on(x, y, z) {
-    
-}
+
