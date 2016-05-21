ab3660392e2fae2b6bce3bf44db5b7b07e288dda
Tina NKH <tina.nikoukhah@gmail.com>
2016-05-22 00:34:35 +0200

    Class Buffer skeleton

---
+++ src/Buffer.cpp
@@ -0,0 +1,15 @@
+#include "Buffer.h"
+
+/**
+ * @brief Default Constructor
+ */
+Buffer::Buffer() {
+    std::perror("Not yet implemented");
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * @brief Destructor
+ */
+Buffer::~Buffer() {
+}
