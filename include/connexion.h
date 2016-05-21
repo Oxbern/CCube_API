8bf3f3dd33cee5519a61217eb42f4e960c7a9f7b
Yann Lachever <lachevery@gmail.com>
2016-05-21 18:01:04 +0200

    Added a function to encode message before to transmit data

--- include/connexion.h
+++ include/connexion.h
@@ -1,6 +1,9 @@
 #ifndef CONNEXION_H
 #define CONNEXION_H
 
+#include <stdint.h>
+#include "cube.h"
+
 /**
  * File descriptor to communicate with the device
  */
@@ -13,5 +16,12 @@ extern int fd;
  * @return file descriptor for communicate with the device
  */
 int open_connection(char* dev);
+
+int getNumberOfBuffer(uint16_t size);
+
+buffer* data_encoding(uint8_t codop, uint16_t size, uint8_t *data);
+
+uint8_t* data_decoding(uint8_t *data, int length);
+
 
 #endif
