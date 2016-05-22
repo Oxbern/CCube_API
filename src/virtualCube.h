b158ff924ddf0cd79730180e2f141d6eab9f7731
Benjamin LRX <benjamin.l@hotmail.fr>
2016-05-22 12:54:36 +0200

    Add ACK message CMD

--- src/virtualCube.h
+++ src/virtualCube.h
@@ -5,6 +5,9 @@
 
 #define CDC_DISPLAY_CUBE 0x01
 #define CDC_SEND_ACK 0x10
+#define CDC_SEND_ACK_OK 0x11
+#define CDC_SEND_ACK_ERR 0x12
+#define CDC_SEND_ACK_NOK 0x13
 
 
 void CDC_Receive_FS (uint8_t *buff_RX, uint32_t *Len);
