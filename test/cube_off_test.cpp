3a44e049024cd4c24030fce8894f9287571243e2
Yann Lachever <lachevery@gmail.com>
2016-05-23 18:43:47 +0200

    Simple file to test the off(x,y,z) and off() methods

--- test/cube_off_test.cpp
+++ test/cube_off_test.cpp
@@ -5,7 +5,7 @@
 #include "Cube.h"
 
 /**
-    File to test the behaviour of the on(..) function 
+    File to test the behaviour of the off(..) function 
  */
 
 int main(int argc, char *argv[]) {
@@ -14,27 +14,35 @@ int main(int argc, char *argv[]) {
 
 /*******************************************************************
                     TEST 1 : all leds switched off
-********************************************************************/    
-    //LedBuffer 1 : all led switched off
-    uint16_t ledBuffer1[10][10];
+********************************************************************/
+    uint16_t res_expected[10][10];
     for (int z = 0; z < 10; z++) {
         for (int y = 0; y < 10; y++) {
             if (z == 9) {
-                    ledBuffer1[z][y] = (uint16_t)0x0;
+                    res_expected[z][y] = (uint16_t)0x0;
             } else {
                 if (y==9)     
-                    ledBuffer1[z][y] = (uint16_t)(1 << z);
+                    res_expected[z][y] = (uint16_t)(1 << z);
                 else
-                    ledBuffer1[z][y] = (uint16_t)0;
+                    res_expected[z][y] = 0;
             }
         }
     }
 
     Cube cube1;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            for (int k = 0; k < 9; k++) {
+                cube1.on(k, j, i);
+                cube1.off(k, j, i);
+            }
+        }
+    }
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
-                led = (uint8_t)((ledBuffer1[i][j] >> k) & 0x1);
+                led = (uint8_t)((res_expected[i][j] >> k) & 0x1);
 
                 if (!(led == cube1.getLedBuffer(k, j, i))) {
                     perror("Test 1 : FAILED");
@@ -45,38 +53,36 @@ int main(int argc, char *argv[]) {
     }
     printf("Test 1 : PASSED\n");
 
-
 /*******************************************************************
-                    TEST 2 : all leds switched on
+                    TEST 2 : one led in two switched on
 ********************************************************************/
-    //LedBuffer 2 : all led switched on
-    uint16_t ledBuffer2[10][10];
     for (int z = 0; z < 10; z++) {
         for (int y = 0; y < 10; y++) {
             if (z == 9) {
-                    ledBuffer2[z][y] = (uint16_t)0x0;
+                    res_expected[z][y] = (uint16_t)0x0;
             } else {
                 if (y==9)     
-                    ledBuffer2[z][y] = (uint16_t)(1 << z);
+                    res_expected[z][y] = (uint16_t)(1 << z);
                 else
-                    ledBuffer2[z][y] = (uint16_t)0b0000000111111111;
+                    res_expected[z][y] = 0b101010101;
             }
         }
     }
 
-
     Cube cube2;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            for (int k = 0; k < 9; k++)
-                cube2.on(k, j, i);
+            for (int k = 0; k < 9; k++) {
+                if (k % 2 == 0)
+                    cube2.on(k, j, i);
+            }
         }
     }
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
-                led = (uint8_t)((ledBuffer2[i][j] >> k) & 0x1);
+                led = (uint8_t)((res_expected[i][j] >> k) & 0x1);
 
                 if (!(led == cube2.getLedBuffer(k, j, i))) {
                     perror("Test 2 : FAILED");
@@ -87,51 +93,48 @@ int main(int argc, char *argv[]) {
     }
     printf("Test 2 : PASSED\n");
 
+
 /*******************************************************************
-                    TEST 3 : one led in two switched on
+                    TEST 3 : test off without argument
 ********************************************************************/
-    //LedBuffer 3 : all led switched on
-    uint16_t ledBuffer3[10][10];
     for (int z = 0; z < 10; z++) {
         for (int y = 0; y < 10; y++) {
             if (z == 9) {
-                    ledBuffer3[z][y] = (uint16_t)0x0;
+                    res_expected[z][y] = (uint16_t)0x0;
             } else {
                 if (y==9)     
-                    ledBuffer3[z][y] = (uint16_t)(1 << z);
+                    res_expected[z][y] = (uint16_t)(1 << z);
                 else
-                    ledBuffer3[z][y] = (uint16_t)0b0000000101010101;
+                    res_expected[z][y] = 0;
             }
         }
     }
-
 
     Cube cube3;
-    bool b = true;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            b = true;
             for (int k = 0; k < 9; k++) {
-                if (b)
                     cube3.on(k, j, i);
-                b = !b;
             }
         }
     }
+
+    //Switch off all the leds
+    cube3.off();
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
-                led = (uint8_t)((ledBuffer3[i][j] >> k) & 0x1);
+                led = (uint8_t)((res_expected[i][j] >> k) & 0x1);
 
                 if (!(led == cube3.getLedBuffer(k, j, i))) {
-                    perror("Test 3 : FAILED");
+                    perror("Test 2 : FAILED");
                     return EXIT_FAILURE;
                 }
             }
         }
     }
     printf("Test 3 : PASSED\n");
-
+ 
     return EXIT_SUCCESS;
 }
