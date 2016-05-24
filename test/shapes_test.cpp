9f9a09fed1e7815733d9ff567270f72df5d1f166
Jonathan Pucci <jonathanpucci06@gmail.com>
2016-05-24 19:42:18 +0200

    test all leds for Pierre

--- test/shapes_test.cpp
+++ test/shapes_test.cpp
@@ -23,45 +23,47 @@ int main(int argc, char *argv[]) {
                 cube.off(i,j,k);
             }
         }
-    }
+     }
+    // cube.on(6,6,3);
+    // cube.display("/dev/ttyACM0");
     //cube.on(i,3,3);
     //cube.on(1,1,1);
     char const *dev = "/dev/ttyACM0";
     char const *loc = "local";
      
-    cube.display(dev );
+    //cube.display(dev );
 
 
-    // Shape sphere(&cube);
-    // int r = 2;
-    // Point p(3,3,3);
-    // sphere.loadSphereShape(r,p,false);
-    // sphere.display("/dev/ttyACM0");
-    // sleep(1);
+    Shape sphere(&cube);
+    int r = 2;
+    Point p(3,3,3);
+    sphere.loadSphereShape(r,p,false);
+    sphere.display("/dev/ttyACM0");
+    sleep(1);
 
-    // sphere.loadSphereShape(r,p,true);
-    // sphere.display(dev);
-    // sleep(1);
+    sphere.loadSphereShape(r,p,true);
+    sphere.display(dev);
+    sleep(1);
 
-    // Vec3 v(0,3,0);
-    // sphere.translateShape(v);
-    // sphere.display(dev);
-    // sleep(1);
+    Vec3 v(0,3,0);
+    sphere.translateShape(v);
+    sphere.display(dev);
+    sleep(1);
 
-    // Point ori(0,0,1);
-    // sphere.loadCircleShape(r,p,ori,false);
-    // sphere.display(dev);
-    // sleep(1);
+    Point ori(0,0,1);
+    sphere.loadCircleShape(r,p,ori,false);
+    sphere.display(dev);
+    sleep(1);
 
-    // sphere.loadCircleShape(r,p,ori,true);
-    // sphere.display(dev);
-    // sleep(1);
+    sphere.loadCircleShape(r,p,ori,true);
+    sphere.display(dev);
+    sleep(1);
 
-    // sphere.loadCubeShape(r,p,true);
-    // sphere.display(dev);
-    // sleep(1);
+    sphere.loadCubeShape(r,p,true);
+    sphere.display(dev);
+    sleep(1);
 
-    // sphere.loadOkShape();
-    // sphere.display(dev);
-    // sleep(1);
+    sphere.loadOkShape();
+    sphere.display(dev);
+    sleep(1);
 }
