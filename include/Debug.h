a1452b8efb604d6cb22fa595c017c3a8c1f23b08
Yann Lachever <lachevery@gmail.com>
2016-05-25 10:43:57 +0200

    Debug level set to 0

--- include/Debug.h
+++ include/Debug.h
@@ -1,17 +1,17 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#ifdef DEBUG
-#define DEBUG_TEST 1
-#else
-#define DEBUG_TEST 0
+
+#define LOG_LEVEL 0
+
+#ifndef LOG_LEVEL
+    #define LOG_LEVEL 0
 #endif
 
 /**
  * DEBUG print to display one message
  */
-#define DEBUG(x) do { \
-    if (DEBUG_TEST) { std::cerr << x << std::endl; } \
-} while (0)
+#define log(level, x) do { \
+if (LOG_LEVEL && (LOG_LEVEL >= level)) { std::cerr << x << std::endl; } } while (0)
 
 #endif //DEBUG_H
