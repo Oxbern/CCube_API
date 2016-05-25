fa29021b31a0a3d55801fedff9345942f26378cd
Tina NKH <tina.nikoukhah@gmail.com>
2016-05-25 18:10:23 +0200

    non regression test
    valgrind ok

--- test/Message_test.cpp
+++ test/Message_test.cpp
@@ -1,31 +1,28 @@
 #include <cstdio>
 #include <cstdlib>
-#include <unistd.h>
 #include <iostream>
 
-#include "Utils.h"
-#include "Buffer.h"
-#include "Message.h"
-#include "Ack.h"
+#include "DataMessage.h"
+#include "AckMessage.h"
 
 int main(int argc, char *argv[]) {
     Message message;
 
-    Message mess(132, 2);
+    DataMessage dataMess(12, 0);
     uint8_t *data = new uint8_t[92];
     for (int i = 0 ; i < 92; i++)
         data[i] = i;
-    mess.encode(data, 92);
+    dataMess.encode(data, 92);
 
     delete [] data;
 
-    Ack ack;
-    Ack ack2(ACK_OK, BUFF_SENDING, 32, 12);
-    if (ack2.checkAck(12))
+    AckMessage ack;
+    AckMessage ack2(ACK_OK);
+    if (ack2.checkAck(0))
         std::cout << "The crc matches : " << ack2.getCrc() << "\n";
 
     Buffer buff;
-    Buffer buff2(1, LIGHT_SENDING, 13, 22);
+    Buffer buff2(SIZE_BUFFER, 1, LIGHT_SENDING, 13, 22);
     if (buff2.getHeader() == 1)
         std::cout << "It is the first buffer \n";
     
