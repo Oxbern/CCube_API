9d0d50690c85c111e95f9f820eb4d9ef2ba11f49
Yann Lachever <lachevery@gmail.com>
2016-05-26 12:50:25 +0200

    New test added for try to write and read in file

---
+++ test/DeviceFile_test2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+
+#include "Device.h"
+
+
+int main(int argc, char *argv[]) {
+    int size = 9;
+    Device dev("/dev/stdin", "NoId", size, size, size);
+
+
+    int sizeAckData = SIZE_ACK - SIZE_OPCODE - SIZE_CRC - SIZE_SIZE - 1;
+    uint8_t data[3] = {1,2,3};
+
+    Message m (SIZE_BUFFER, sizeAckData, ACK_OK);
+    m.encode(data);
+
+    dev.send(m);
+
+    char *buffer = new char [7];
+    buffer[6] = '\0';
+    std::fstream &file = dev.getFile();
+
+    if (file.is_open()) {
+        std::cout << std::endl;
+        file.read(buffer, 6);
+        std::cout << buffer << std::endl;
+    }
+
+    delete []buffer;
+
+    dev.disconnect();
+
+    return EXIT_SUCCESS;
+}
