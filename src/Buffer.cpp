118d0a153f4be19abc21d1f86df0c486835a706b
Yann Lachever <lachevery@gmail.com>
2016-05-26 15:27:17 +0200

    Method toArray in Buffer implemented

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -5,7 +5,7 @@
 /**
  * @brief Creates a buffer with no data place
  */
-Buffer::Buffer() : sizeBuffer(0), opCode(0), sizeLeft(0), crc(0), header(0)
+Buffer::Buffer() : header(0), sizeBuffer(0), opCode(0), sizeLeft(0), crc(0)
 {
     data = new uint8_t[0];
 }
@@ -13,7 +13,7 @@ Buffer::Buffer() : sizeBuffer(0), opCode(0), sizeLeft(0), crc(0), header(0)
 /**
  * @brief Creates a right sized buffer
  */
-Buffer::Buffer(int sizeBuff) : sizeBuffer(sizeBuff), opCode(0), sizeLeft(0), crc(0), header(0)
+Buffer::Buffer(int sizeBuff) : header(0), sizeBuffer(sizeBuff), opCode(0), sizeLeft(0), crc(0)
 {
     data = new uint8_t[sizeBuff-DATA_INDEX - SIZE_CRC];
 }
@@ -26,7 +26,7 @@ Buffer::Buffer(int sizeBuff) : sizeBuffer(sizeBuff), opCode(0), sizeLeft(0), crc
  * @param crcCheck
  */
 Buffer::Buffer(int sizeBuff, uint8_t head, uint8_t code, uint16_t size, uint16_t crcCheck) :
-    sizeBuffer(sizeBuff), opCode(code), sizeLeft(size), crc(crcCheck), header(head)
+        header(head), sizeBuffer(sizeBuff), opCode(code), sizeLeft(size), crc(crcCheck)
 {
     data = new uint8_t[sizeBuff - DATA_INDEX - SIZE_CRC];
 }
@@ -43,17 +43,12 @@ Buffer::~Buffer()
     data = NULL;
 }
 
-int sizeData(int sizeBuffer) {
+int dataSize(int sizeBuffer) {
     return (sizeBuffer - DATA_INDEX - SIZE_CRC);
 }
 
-/**
- * @todo choose between Buffer.toArray, Message.toArray, both or neither
- * @brief Converts a buffer into an array
- * @param buffLinear the filled array
- */
-void Buffer::toArray(uint8_t* buffLinear)
-{
+int crcIndex(int sizeBuffer) {
+    return (sizeBuffer - SIZE_CRC);
 }
 
 /**
@@ -63,6 +58,7 @@ void Buffer::toArray(uint8_t* buffLinear)
 void Buffer::setHeader(uint8_t head) {
     this->header = head;
 }
+
 /**
  * @brief Sets the opCode
  * @param code
@@ -70,7 +66,6 @@ void Buffer::setHeader(uint8_t head) {
 void Buffer::setOpCode(uint8_t code) {
     this->opCode = code;
 }
-
 /**
  * @brief Sets the sizeLeft
  * @param size
@@ -90,7 +85,7 @@ void Buffer::setCrc(uint16_t crcCheck) {
 /**
  * @brief Sets the data
  * @param index
- * @param dataIndex 
+ * @param dataIndex
  */
 void Buffer::setData(int index, uint8_t dataIndex) {
     this->data[index] = dataIndex;
@@ -147,13 +142,43 @@ bool Buffer::operator==(Buffer b){
                 this->crc == b.crc &&
                 this->sizeBuffer == b.sizeBuffer);
     if (ret) {
-        for (int i = 0; i < (this->sizeBuffer - DATA_INDEX - SIZE_CRC); i ++) {            
-            ret = (this->data[i] == b.getData()[i]);            
+        for (int i = 0; i < (this->sizeBuffer - DATA_INDEX - SIZE_CRC); i ++) {
+            ret = (this->data[i] == b.getData()[i]);
         }
     }
     return ret;
 }
 
+/**
+ * @todo choose between Buffer.toArray, Message.toArray, both or neither
+ * @brief Converts a buffer into an array
+ * @param buffLinear the filled array
+ */
+void Buffer::toArray(uint8_t* buffLinear)
+{
+    if (buffLinear != NULL) {
+        buffLinear[HEADER_INDEX] = header;
+        buffLinear[OPCODE_INDEX] = opCode;
+        uint8_t tab[2];
+        convert16to8(sizeLeft, tab);
+        buffLinear[SIZE_INDEX] = tab[0];
+        buffLinear[SIZE_INDEX + 1] = tab[1];
+
+        //Convert data
+        for (int i = 0; i < dataSize(sizeBuffer); i++)
+            buffLinear[DATA_INDEX + i] = data[i];
+
+        //split crc into two uint8_t
+        int indexCrc = crcIndex(sizeBuffer);
+        convert16to8(crc, tab);
+        buffLinear[indexCrc] = tab[0];
+        buffLinear[indexCrc + 1] = tab[1];
+    }
+}
+
+/**
+ * @brief todo
+ */
 std::string Buffer::toString()
 {
     std::ostringstream convert;
@@ -167,7 +192,7 @@ std::string Buffer::toString()
     convert << (int) tab[1];
 
     //Convert data
-    for (int i = 0; i < sizeData(sizeBuffer); i++)
+    for (int i = 0; i < dataSize(sizeBuffer); i++)
         convert << (int)data[i];
 
     //split crc into two uint8_t
