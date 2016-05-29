d7630715b39b95487e00369e62f3945bba57b75a
Yann Lachever <lachevery@gmail.com>
2016-05-29 11:55:57 +0200

    DeviceFile_test2 improved

--- test/DeviceFile_test2.cpp
+++ test/DeviceFile_test2.cpp
@@ -2,53 +2,37 @@
 #include <fstream>
 
 #include "Device.h"
-#include "DeviceShape.h"
 
-
+/**
+ * Simple test to switch on some LEDs using methods of the API
+ */
 int main(int argc, char *argv[]) {
-    Device dev("/dev/ttyACM0", 1);
-    dev.connect();
+    std::string port = "dev/ttyACM0";
+    Device dev(port, 1);
 
-/*
-    int sizeAckData = SIZE_ACK - SIZE_OPCODE - SIZE_CRC - SIZE_SIZE - 1;
-    uint8_t data[3] = {1,2,3};
-    
-    Message m (dev.getID(), SIZE_BUFFER, sizeAckData, ACK_OK);
-    m.encode(data);
-*/
-    DeviceShape cubeLed(9,9,9);
-    cubeLed.on(0,0,7);
+    if (dev.connect()){
+        std::cout << "Connection to " << port << " device successful" << std::endl;
 
-    //std::cout << cubeLed;
+        //Switch on some LEDs
+        if (!dev.on(0, 0, 0) || !dev.on(4, 4, 4) || !dev.on(8, 8, 8)) {
+            std::cout << "TEST FAILED " << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    Message m (dev.getID(), SIZE_BUFFER, (9*9*9)/8, 1);
+        if (!dev.display()) {
+            std::cout << "TEST FAILED " << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    uint8_t  * tabLed = cubeLed.toArray();
-    /*for(int i =0; i < (9*9*9); i++) {
-        //tabLed[364] = 1
-        std::cout << (int) tabLed[i];
-    }
-    std::cout << std::endl;*/
-
-    std::cout << static_cast<void*>(tabLed) << std::endl;
-    m.encode(tabLed);
-    std :: cout << m.toStringDebug();
-
-    dev.send(&m);
-
-    /*char *buffer = new char [7];
-    buffer[6] = '\0';
-    std::fstream &file = dev.getFile();
-
-    if (file.is_open()) {
-        std::cout << std::endl;
-        file.read(buffer, 6);
-        std::cout << buffer << std::endl;
+        if (dev.disconnect()) {
+            std::cout << "Disconnection to " << port << " device successful" << std::endl;
+            std::cout << "TEST PASSED " << std::endl;
+            return EXIT_SUCCESS;
+        } else {
+            std::cout << "Unable to disconnect " << port << std::endl;
+        }
     }
 
-    delete []buffer;*/
-
-    dev.disconnect();
-
-    return EXIT_SUCCESS;
+    std::cout << "TEST FAILED " << std::endl;
+    return EXIT_FAILURE;
 }
