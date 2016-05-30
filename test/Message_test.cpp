a23c681816382265dedef4127dccb179b9d9f0c6
Tina NKH <tina.nikoukhah@gmail.com>
2016-05-30 18:44:43 +0200

    non regression test passed for new message classes

--- test/Message_test.cpp
+++ test/Message_test.cpp
@@ -14,34 +14,52 @@ int main(int argc, char *argv[]) {
 
     Message mess(1, SIZE_BUFFER, 1, 0);
 
-    Buffer b();
-    DataMessage dataMess(2, 12, 0);
+    DataMessage dataMess(2, 92, 0);
     uint8_t *data = new uint8_t[92];
     for (int i = 0 ; i < 92; i++)
         data[i] = i;
     dataMess.encode(data);
-
-    delete [] data;
+    std::cout << "\n DataMess\n";
+    std::cout << dataMess.toStringDebug();
 
     AckMessage ack();
     AckMessage ack2(2, ACK_OK);
+    std::cout << "\n Ack2\n";
+    std::cout << ack2.toStringDebug();
 
+    ack2.encode(data);
+    std::cout << "\n Ack2 encoded : \n";
+    std::cout << ack2.toStringDebug();    
+    
     Buffer buff;
     Buffer buff2(SIZE_BUFFER);
     if (buff2.getHeader() == 1)
         std::cout << "It is the first buffer \n";
-
+    
     RequestMessage req();
     RequestMessage req2(3, BUFF_ASKING);
-
+    std::cout << "\n Req2 \n";
+    std::cout << req2.toStringDebug();    
+    
     AnswerMessage ans();
     AnswerMessage ans2(3, LIGHT_RECEPTION);
+    std::cout << "\n Ans2\n";
+    std::cout << dataMess.toStringDebug();    
 
     SetMessage set();
     SetMessage set2(4, LIGHT_SENDING);
+    std::cout << "\n Set2\n";
+    std::cout << set2.toStringDebug();    
 
     FirstMessage one();
     FirstMessage one2(4, DEVICE_INFO);
+    std::cout << " \n One2\n";
+    std::cout << one2.toStringDebug();    
+       one2.encode(data);
+    std::cout << " \n One2 encoded :\n";
+    std::cout << one2.toStringDebug();    
+
+    delete [] data;
     
     std::cout << "PASSED \n";
     return 0;
