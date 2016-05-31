12b0906c21e30366d7d6e63c7117555cd34d9c6f
Yann Lachever <lachevery@gmail.com>
2016-05-31 15:53:07 +0200

    Log messages added in Device

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <fstream>
+#include <errno.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -89,7 +90,7 @@ bool Device::connect()
 	    	fcntl(fd, F_SETFL, 0);
     }
 
-    LOG(1, "Device " + std::string((fd ? "connected" : "not connected")));
+    LOG(1, "Device " + std::string((fd >= 0 ? "" :  "not ")) + "connected");
     return (fd >= 0);
 }
 
@@ -104,7 +105,7 @@ bool Device::disconnect()
         fd = -1;
     }
 
-    LOG(1, "Device disconnected");
+    LOG(1, "Device " + std::string((fd == -1 ? "" :  "not ")) + "disconnected");
     return (fd == -1);
 }
 
@@ -170,27 +171,19 @@ bool Device::askForDisplaySize()
 bool Device::writeToFileDescriptor(uint8_t* data, int dataSize)
 {
     if (fd) {
-        LOG(2, "Buffer send (size = " + std::to_string(dataSize)
+        LOG(2, "Trying to write Buffer (size = " + std::to_string(dataSize)
             + " Bytes) : " + uint8ArrayToString(data, dataSize));
 
         if (write(fd, (char *) data, dataSize)) {
-            LOG(1, "Data written to file");
-
-            uint8_t ack[10] = {0};
-            read(fd, &ack[0], 10);
-
-            fprintf(stdout, "ACK: ");
-            for (int i = 0; i < 10; ++i)
-	            fprintf(stdout, "%u |", ack[i]);
-            fprintf(stdout, "\n");
+            LOG(2, "Data written to file");
 
             return true;
         } else {
-            LOG(1, "Error while writing data to file");
+            LOG(2, "Error while writing data to file : " + std::string(std::strerror(errno)));
         }
 
     } else {
-        LOG(1, "Unable to write data to file");
+        LOG(2, "Unable to write data to file : wrong file descriptor");
     }
     return false;
 }
@@ -200,43 +193,48 @@ bool Device::writeToFileDescriptor(uint8_t* data, int dataSize)
  */
 bool Device::send(Message* mess)
 {
-    LOG(1, "Sending message");
-    if (!fd) {
-        while (!connect()) {
-            //TODO Timeout
-            continue;
-        }
-    }
-    int n = mess->NbBuffers();
-    for (int i = 0; i < n; i++) {
-        int sizeBuffer = mess->getBuffer()[i].getSizeBuffer();
-        uint8_t * buffString = new uint8_t[sizeBuffer];
-        mess->getBuffer()[i].toArray(buffString);
-
-        LOG(3, mess->toStringDebug());
-
-        if ((this->port.compare("/dev/stdin") == 0) || (this->port.compare("/dev/stdout") == 0)) {
-            //VirtualCube
-            LOG(2, "Buffer send (size = " + std::to_string(sizeBuffer)
-                + " Bytes) : " + uint8ArrayToString(buffString, sizeBuffer));
-
-            LOG(1, "Virtual sending");
-            //Virtual sending
-            // uint8_t* buffer = CDC_Receive_FS(buffString);
-            // delete []buffer;
-
-        } else {
-            
-            while (!writeToFileDescriptor(buffString, sizeBuffer)) {
-	            continue;
+    LOG(1, "Trying to send a message");
+    if (mess != NULL) {
+        if (!fd) {
+            while (!connect()) {
+                //TODO Timeout
+                continue;
             }
-            
         }
-        delete []buffString;
-    }
-    LOG(1, "Message sended");
+        int n = mess->NbBuffers();
+        for (int i = 0; i < n; i++) {
+            int sizeBuffer = mess->getBuffer()[i].getSizeBuffer();
+            uint8_t *buffString = new uint8_t[sizeBuffer];
+            mess->getBuffer()[i].toArray(buffString);
 
-    return true;
+            LOG(3, mess->toStringDebug());
+
+            if ((this->port.compare("/dev/stdin") == 0) || (this->port.compare("/dev/stdout") == 0)) {
+                //VirtualCube
+                LOG(2, "Buffer send (size = " + std::to_string(sizeBuffer)
+                       + " Bytes) : " + uint8ArrayToString(buffString, sizeBuffer));
+
+                LOG(1, "Virtual sending");
+                //Virtual sending
+                // uint8_t* buffer = CDC_Receive_FS(buffString);
+                // delete []buffer;
+
+            } else {
+
+                while (!writeToFileDescriptor(buffString, sizeBuffer)) {
+                    continue;
+                }
+
+
+            }
+            delete[]buffString;
+        }
+        LOG(1, "Message sended");
+
+        return true;
+    }
+    LOG(1, "Unable to send a message to the device");
+    return false;
 }
 
 /**
@@ -309,7 +307,7 @@ bool Device::handleAck(Message mess, AckMessage ack)
 {
     //Check the AckMessage
     if (ack.getOpCode() != ACK_OK) {
-        LOG(3, "Handle a ACK_NOK or ACK_ERR");
+        LOG(3, "Handle an ACK_NOK or ACK_ERR");
         uint8_t ackDataOpcode = ack.getBuffer()[0].getData()[0];
         uint16_t ackDataSize = convertTwo8to16(ack.getBuffer()[0].getData()[1], ack.getBuffer()[0].getData()[2]);
         uint8_t buff[mess.getSizeBuffer()];
