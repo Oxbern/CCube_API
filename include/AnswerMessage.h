9703e8b246308190edad2747e32cf93d677dc397
Tina NKH <tina.nikoukhah@gmail.com>
2016-05-31 23:00:09 +0200

    Answer type messages documention normalised

--- include/AnswerMessage.h
+++ include/AnswerMessage.h
@@ -1,15 +1,36 @@
 #ifndef ANSWERMESSAGE_H
 #define ANSWERMESSAGE_H
 
-#include "Message.h"
-/**
- * @class Message creation when answering to a question
+/*!
+ * \file AnswerMessage.h
+ * \brief Messages of type answer : LIGHT_RECEPTION, SCREEN_SIZE_RECEPTION, 
+ * FIRMWARE_VERSION_RECEPTION, ID_RECEPTION
+ * \version 0.1
  */
+#include "Message.h"
 
+/*! 
+ * \class AnswerMessage
+ * \brief Class representing a message of type Answer
+ */
 class AnswerMessage : public Message
 {
  public :
+    /*!
+     * \brief Constructor
+     *
+     * Creates an answer
+     *
+     * \param id device's ID
+     * \param opCode LIGHT_RECEPTION, SCREEN_SIZE_RECEPTION, 
+     * FIRMWARE_VERSION_RECEPTION, ID_RECEPTION
+     */   
     AnswerMessage(uint8_t id, uint8_t code);
+    
+    /*!
+     * \brief Destructor
+     *
+     */
     ~AnswerMessage();
 };
 
--- src/AnswerMessage.cpp
+++ src/AnswerMessage.cpp
@@ -1,17 +1,22 @@
 #include "AnswerMessage.h"
 
-/**
- * @brief Creates a request
- * @param idDevice
- * @param opCode
+/*!
+ * \brief Constructor
+ *
+ * Creates an answer
+ *
+ * \param id device's ID
+ * \param opCode LIGHT_RECEPTION, SCREEN_SIZE_RECEPTION, 
+ * FIRMWARE_VERSION_RECEPTION, ID_RECEPTION
  */
 AnswerMessage::AnswerMessage(uint8_t id, uint8_t opCode) : Message(id, SIZE_ANSWER, SIZE_ANSWER - DATA_INDEX - SIZE_CRC, opCode)
 {
     LOG(1, "AnswerMessage(idDevice, opCode)");    
 }
 
-/**
- * @brief Destructor
+/*!
+ * \brief Destructor
+ *
  */
 AnswerMessage::~AnswerMessage()
 {
