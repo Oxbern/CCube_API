904b81ebc9622a13b1eceaca37d8d509018cda7c
Tina NKH <tina.nikoukhah@gmail.com>
2016-06-01 12:38:51 +0200

    DataMessage doxygen

--- include/DataMessage.h
+++ include/DataMessage.h
@@ -1,16 +1,34 @@
 #ifndef DATAMESSAGE_H
 #define DATAMESSAGE_H
 
+/*!
+ * \file DataMessage.h
+ * \brief Messages of type Data : BUFF_SENDING, FIRMWARE_UPDATE_SENDING
+ * \version 0.1
+ */
 #include "Message.h"
 
-/**
- * @class Message creation when sending data
+/*! 
+ * \class DataMessage
+ * \brief Class representing a message of type Data
  */
-
 class DataMessage : public Message
 {
  public :
+    /*!
+     * \brief Constructor
+     *
+     * Creates a message with a list of buffers
+     *
+     * \param idDevice device's ID
+     * \param size message's size
+     * \param code operation code : BUFF_SENDING, FIRMWARE_UPDATE_SENDING
+     */
     DataMessage(uint8_t id, uint16_t size, uint8_t code);
+    /*!
+     * \brief Destructor
+     *
+     */
     ~DataMessage();
 };
 
--- src/DataMessage.cpp
+++ src/DataMessage.cpp
@@ -1,18 +1,23 @@
 #include "DataMessage.h"
 
-/**
- * @brief Creates a message with list of buffers with opcode and sizeLeft
- * @param size of the message
- * @param code operation code
+/*!
+ * \brief Constructor
+ *
+ * Creates a message with a list of buffers
+ *
+ * \param idDevice device's ID
+ * \param size message's size
+ * \param code operation code : BUFF_SENDING, FIRMWARE_UPDATE_SENDING
  */
-DataMessage::DataMessage(uint8_t idDevice, uint16_t size, uint8_t code) : Message(idDevice, SIZE_BUFFER, size, code)
+DataMessage::DataMessage(uint8_t idDevice, uint16_t size, uint8_t code) :
+    Message(idDevice, SIZE_BUFFER, size, code)
 {
     LOG(1, "DataMessage(idDevice, sizeMessage, opCode)");
 }
 
-
-/**
- * @brief Destructor
+/*!
+ * \brief Destructor
+ *
  */
 DataMessage::~DataMessage()
 {
