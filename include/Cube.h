1342fc9b77da61dcbc5efb9703917f5fac720089
GuillaumeHalb <guillaume.halb@free.fr>
2016-06-01 08:54:42 +0200

    add doxygen on Cube and ShapeToDisplay .cpp .h

--- include/Cube.h
+++ include/Cube.h
@@ -4,27 +4,104 @@
  * \class Cube
  * \brief Class describing a cube (a shape which can be displayed on the device)
  */
-class Cube : public ShapeToDisplay
-{
- public:
-    Cube(int i = 0, Point p = Point (0, 0, 0), bool b = true, 
+class Cube : public ShapeToDisplay {
+public:
+    /*! 
+     * \brief Constructor
+     * Constructor of the class Cube
+     */
+    Cube(int i = 0, Point p = Point(0, 0, 0), bool b = true,
             int x = 9, int y = 9, int z = 9);
-    
+
+    /*!
+     * \brief Destructor
+     * Destructor of the class ShapeToDisplay
+     */
     virtual ~Cube();
 
+    /*!
+     * \fn void init()
+     * \brief Initialises the device depending 
+     * on the parameters of the shape (the cube here)
+     */
     void init();
 
+    /*! 
+     * \fn virtual bool incrSize()
+     * \brief Increases the size of the shape 
+     * \return true if the bigger cube stays in the 3D array 
+     * false otherwise
+     */
     bool incrSize();
+
+    /*! 
+     * \fn virtual bool decrSize()
+     * \brief Decreases the size of the shape 
+     * \return true if the size does not become negative
+     * false otherwise
+     */
     bool decrSize();
 
+    /*! 
+     * \fn bool moveUp()
+     * \brief Moves the cube up in the 3D array 
+     * \return true if the whole cube stays in the 3D array
+     * false otherwise
+     */
     bool moveUp();
+
+    /*! 
+     * \fn bool moveDown()
+     * \brief Moves the cube down in the 3D array 
+     * \return true if the whole cube stays in the 3D array
+     * false otherwise
+     */
     bool moveDown();
+
+    /*! 
+     * \fn bool moveLeft()
+     * \brief Moves the cube on the left in the 3D array 
+     * \return true if the whole cube stays in the 3D array
+     * false otherwise
+     */
     bool moveLeft();
+
+    /*! 
+     * \fn bool moveRight()
+     * \brief Moves the cube on the right in the 3D array 
+     * \return true if the whole cube stays in the 3D array
+     * false otherwise
+     */
     bool moveRight();
+
+    /*! 
+     * \fn bool moveForward()
+     * \brief Moves the cube forward in the 3D array 
+     * \return true if the whole cube stays in the 3D array
+     * false otherwise
+     */
     bool moveForward();
+
+    /*! 
+     * \fn bool moveBackward()
+     * \brief Moves the cube backward in the 3D array 
+     * \return true if the whole cube all stays in the 3D array
+     * false otherwise
+     */
     bool moveBackward();
 
+    /*! 
+     * \fn void print(std::ostream &str)
+     * \brief Calls the print function of the superclass (ShapeToDisplay)
+     */
     void print(std::ostream &str);
 };
 
-// std::ostream& operator<<(std::ostream &out, Cube &c);
+/*!
+ * \fn std::ostream& operator<<(std::ostream &out, Cube &c)
+ * \brief Overloads of the operator <<
+ * @param out : stream on which we write
+ * @param c : Cube shape we want to display
+ * \return std::ostream Out
+ */
+std::ostream& operator<<(std::ostream &out, Cube &c);
--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,86 +1,92 @@
 #include "Cube.h"
 #include <sstream>
 
-/**
- * @brief TODO
+/*! 
+ * \brief Constructor
+ * Constructor of the class Cube
  */
-Cube::Cube(int i, Point p, bool b, int x, int y, int z) : ShapeToDisplay(i, p, b, x, y, z)
+Cube::Cube(int i, Point p, bool b, int x, int y, int z) 
+: ShapeToDisplay(i, p, b, x, y, z) 
 {
-    LOG(1,"Cube(int i, Point p, bool b, bool ***s, int x, int y, int z)");
+    LOG(1, "Cube(int i, Point p, bool b, bool ***s, int x, int y, int z)");
     init();
 }
 
-/**
- * \fn void Cube::init()
- * \brief Function thaht initialise the device in terms 
- * of the parameters of the shape (the cube here)
+/*!
+ * \fn void init()
+ * \brief Function that initialises the device depending 
+ * on the parameters of the shape (the cube here)
  */
-void Cube::init()
+void Cube::init() 
 {
     for (int x = 0; x < sizeX; x++) {
-	for (int y = 0; y < sizeY; y++) {	    
-	    for (int z = 0; z < sizeZ; z++) {
-		if (full) 
-		    {
-			if (x >= origin.getX() && x < origin.getX() + size
-			    && y >= origin.getY() && y < origin.getY() + size
-			    && z >= origin.getZ() && z < origin.getZ() + size) 
-			    on(x, y, z);
-		    }
-		else		    
-		    if (x == origin.getX() || x == origin.getX() + size - 1
-			|| y == origin.getY() || y == origin.getY() + size - 1
-			|| z == origin.getZ() || z == origin.getZ() + size - 1) 
-			on(x, y, z);	
-	    }
-	}
+        for (int y = 0; y < sizeY; y++) {
+            for (int z = 0; z < sizeZ; z++) {
+                if (full) {
+                    if (x >= origin.getX() && x < origin.getX() + size
+                            && y >= origin.getY() && y < origin.getY() + size
+                            && z >= origin.getZ() && z < origin.getZ() + size)
+                        on(x, y, z);
+                } else
+                    if (x == origin.getX() || x == origin.getX() + size - 1
+                        || y == origin.getY() || y == origin.getY() + size - 1
+                        || z == origin.getZ() || z == origin.getZ() + size - 1)
+                    on(x, y, z);
+            }
+        }
     }
 }
 
-
-Cube::~Cube()
+/*!
+ * \brief Destructor
+ * Destructor of the class ShapeToDisplay
+ */
+Cube::~Cube() 
 {
-    LOG(1,"~Cube()");
+    LOG(1, "~Cube()");
 }
 
-/**
- * \fn bool Cube::incrSize()
- * \brief Function that increases the size of the cube  
- * of the parameters of the shape (the cube here)
+/*! 
+ * \fn virtual bool incrSize()
+ * \brief Increases the size of the shape 
+ * \return true if the bigger cube stays in the 3D array 
+ * false otherwise
  */
 bool Cube::incrSize() 
 {
     int X = origin.getX() + size;
     int Y = origin.getY() + size;
     int Z = origin.getZ() + size;
-    
+
     std::stringstream s;
-    
+
     s << "X = " << X << ", Y = " << Y << ", Z = " << Z;
 
     LOG(1, s.str());
-    
-    if (origin.getX() + size == 9 
-	|| origin.getY() + size == 9 
-	|| origin.getZ() + size == 9) 
-	{
-	    std::cout << "Cube size cannot increase more" << std::endl;
-	    return false;
-	}
+
+    if (origin.getX() + size == 9
+            || origin.getY() + size == 9
+            || origin.getZ() + size == 9) {
+        std::cout << "Cube size cannot increase more" << std::endl;
+        return false;
+    }
     size++;
     off();
     init();
     return true;
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn virtual bool decrSize()
+ * \brief Decreases the size of the shape 
+ * \return true if the size does not become negative
+ * false otherwise
  */
 bool Cube::decrSize() 
 {
     if (size == 0) {
-	std::cout << "Cube size cannot decrease more" << std::endl;
-	return false;
+        std::cout << "Cube size cannot decrease more" << std::endl;
+        return false;
     }
     size--;
     off();
@@ -88,133 +94,137 @@ bool Cube::decrSize()
     return true;
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveUp()
+ * \brief Moves the cube up in the 3D array 
+ * \return true if the whole cube stays in the 3D array
+ * false otherwise
  */
 bool Cube::moveUp() 
 {
-    if (origin.getZ() + size < 9)
-	{
-	    off();
-	    origin.incrZ();
-	    init();
-	    return true;
-	}
-    else
-	{
-            std::cout << "Cannot go higher" << std::endl; 
-            return false;
-	}
+    if (origin.getZ() + size < 9) {
+        off();
+        origin.incrZ();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot go higher" << std::endl;
+        return false;
+    }
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveDown()
+ * \brief Moves the cube down in the 3D array 
+ * \return true if the whole cube stays in the 3D array
+ * false otherwise
  */
 bool Cube::moveDown() 
 {
-    if (origin.getZ() > 0)
-	{
-	    off();
-	    origin.decrZ();
-	    init();
-	    return true;
-	}
-    else 
-	{
-	    std::cout << "Cannot go deeper" << std::endl; 
-	    return false;
-	}
+    if (origin.getZ() > 0) {
+        off();
+        origin.decrZ();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot go deeper" << std::endl;
+        return false;
+    }
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveLeft()
+ * \brief Moves the cube on the left in the 3D array 
+ * \return true if the whole cube stays in the 3D array
+ * false otherwise
  */
 bool Cube::moveLeft() 
 {
-    if (origin.getY()  > 0)
-	{
-	    off();
-	    origin.decrY();
-	    init();
-	    return true;
-	}
-    else
-	{
-	    std::cout << "Cannot move more on the left" << std::endl; 
-	    return false;
-	}
+    if (origin.getY() > 0) {
+        off();
+        origin.decrY();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot move more on the left" << std::endl;
+        return false;
+    }
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveRight()
+ * \brief Moves the cube on the right in the 3D array 
+ * \return true if the whole cube stays in the 3D array
+ * false otherwise
  */
-bool Cube::moveRight() 
+bool Cube::moveRight()
 {
-    if (origin.getY() + size < 9)
-	{
-	    off();
-	    origin.incrY();
-	    init();
-	    return true;
-	}
-    else 
-	{
-	    std::cout << "Cannot move more on the right" << std::endl; 
-	    return false;
-	}
+    if (origin.getY() + size < 9) {
+        off();
+        origin.incrY();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot move more on the right" << std::endl;
+        return false;
+    }
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveForward()
+ * \brief Moves the cube forward in the 3D array 
+ * \return true if the whole cube stays in the 3D array
+ * false otherwise
  */
 bool Cube::moveForward() 
 {
-    if (origin.getX() + size < 9)
-	{
-	    off();
-	    origin.incrX();
-	    init();
-	    return true;
-	}
-    else
-	{
-	    std::cout << "Cannot move more forward" << std::endl; 
-	    return false;
-	}
+    if (origin.getX() + size < 9) {
+        off();
+        origin.incrX();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot move more forward" << std::endl;
+        return false;
+    }
 }
 
-/**
- * @brief TODO
+/*! 
+ * \fn bool moveBackward()
+ * \brief Moves the cube backward in the 3D array 
+ * \return true if the whole cube all stays in the 3D array
+ * false otherwise
  */
 bool Cube::moveBackward() 
 {
-    if (origin.getX() > 0)
-	{
-	    off();
-	    origin.decrX();
-	    init();
-	    return true;
-	}
-    else 
-	{
-	    std::cout << "Cannot move more backward" << std::endl; 
-	    return false;
-	}
+    if (origin.getX() > 0) {
+        off();
+        origin.decrX();
+        init();
+        return true;
+    } else {
+        std::cout << "Cannot move more backward" << std::endl;
+        return false;
+    }
 }
 
-
-/**
- * @brief TODO
+/*! 
+ * \fn void print(std::ostream &str)
+ * \brief Calls the print function of the superclass (ShapeToDisplay)
  */
-void Cube::print(std::ostream &str)
+void Cube::print(std::ostream &str) 
 {
     ShapeToDisplay::print(str);
 }
 
-/**
- * @brief TODO
+/*!
+ * \fn std::ostream& operator<<(std::ostream &out, Cube &c)
+ * \brief Overloads of the operator <<
+ * @param out : stream on which we write
+ * @param c : Cube shape we want to display
+ * \return std::ostream Out
  */
-std::ostream& operator<<(std::ostream &out, Cube &c)
+std::ostream& operator<<(std::ostream &out, Cube &c) 
 {
     c.print(out);
     return out;
