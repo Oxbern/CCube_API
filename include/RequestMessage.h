7c174b8aa7b3b7850b7759b86845572e3bc26bc9
Tina NKH <tina.nikoukhah@gmail.com>
2016-06-04 18:18:44 +0200

    RequestMessage class documented

--- include/RequestMessage.h
+++ include/RequestMessage.h
@@ -1,16 +1,44 @@
 #ifndef REQUESTMESSAGE_H
 #define REQUESTMESSAGE_H
 
+/*!
+ * \file RequestMessage.h
+ * \brief Messages of type request : BUFF_ASKING, LIGHT_ASKING, SCREEN_SIZE_ASKING, 
+ * FIRMWARE_VERSION_ASKING, ID_ASKING, AVAILABLE, WHO_ARE_YOU
+ *
+ * \version 0.1
+ */
+
 #include "Message.h"
 
-/**
- * @class Message creation when asking for something
+/*!
+ * \class RequestMessage 
+ * \brief Class representing a message of type request
  */
 class RequestMessage : public Message
 {
  public :
+    /*!
+     * \brief Constructor
+     *
+     * Creates a request
+     *
+     * \param id device's ID
+     * \param opCode BUFF_ASKING, LIGHT_ASKING, SCREEN_SIZE_ASKING, 
+     * FIRMWARE_VERSION_ASKING, ID_ASKING, AVAILABLE, WHO_ARE_YOU
+     */       
     RequestMessage(uint8_t id, uint8_t code);
+
+    /*!
+     * \brief Destructor
+     *
+     */    
     ~RequestMessage();
+
+    /*!
+     * \fn void encodeCrc()
+     * \brief Calculates and sets the crc
+     */
     void encodeCrc();
 };
 
