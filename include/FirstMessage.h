79ab5aa47f22d21609a62e6049db0004d1b01475
Tina NKH <tina.nikoukhah@gmail.com>
2016-06-04 17:42:58 +0200

    class FirstMessage documented

--- include/FirstMessage.h
+++ include/FirstMessage.h
@@ -1,15 +1,35 @@
 #ifndef FIRSTMESSAGE_H
 #define FIRSTMESSAGE_H
 
+/*!
+ * \file FirstMessage.h
+ * \brief First Message sent by the cube to give its personnal info, DEVICE_INFO
+ * \version 0.1
+ */
+
 #include "Message.h"
 
-/**
- * @class Message creation when wanting to set something
+/*!
+ * \class FirstMessage 
+ * \brief Class representing a message with the cube's personnal info
  */
 class FirstMessage : public Message
 {
  public :
+    /*!
+     * \brief Constructor
+     *
+     * Creates an answer
+     *
+     * \param id device's ID
+     * \param opCode DEVICE_INFO
+     */       
     FirstMessage(uint8_t id, uint8_t code);
+
+    /*!
+     * \brief Destructor
+     *
+     */    
     ~FirstMessage();
 };
 
