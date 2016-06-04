6d5b48953223d8dcc6314af41213b0a721a57c5e
Tina NKH <tina.nikoukhah@gmail.com>
2016-06-04 19:22:33 +0200

    documentation Debug

--- include/Debug.h
+++ include/Debug.h
@@ -1,21 +1,21 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
 /*!
  * \file Debug.h
  * \brief Please change LOG_LEVEL to print debug messages
  * \version 0.1
  */
 
-#ifndef DEBUG_H
-#define DEBUG_H
-
-
-#define LOG_LEVEL 0 
+#define LOG_LEVEL 0
 
 #ifndef LOG_LEVEL
 #define LOG_LEVEL 0
 #endif
 
-/**
- * @brief DEBUG print to display one message
+/*!
+ * \def LOG(level, x)
+ * \brief DEBUG print to display one message
  */
 #define LOG(level, x) do {                                              \
         if (LOG_LEVEL && (level >= LOG_LEVEL)) { std::cerr << "\033[1;34m" << x << " \033[0m" << std::endl; } } while (0)
