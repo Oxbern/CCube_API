d5e007072fb3bab420819b342341b418c961cafc
Benjamin LRX <benjamin.l@hotmail.fr>
2016-06-09 21:31:10 +0200

    Update test

--- test/CubicleTests/controller_deviceInfotest.cpp
+++ test/CubicleTests/controller_deviceInfotest.cpp
@@ -12,16 +12,16 @@ int main(int argc, char *argv[])
 {
     Controller c;
 
-    if (c.connectDevice(1)){
+    if (c.connectDevice(1, !C_SECURE)){
         std::cout << "Connection to device successful" << std::endl;
 
         uint8_t *info = c.getDeviceInfo();
 
         std::cout << "size x : " << (int)info[0] << std::endl;
         std::cout << "size y : " << (int)info[1] << std::endl;
-        std::cout << "size z : " << (int)info[2] << std::endl;        
-        
-        
+        std::cout << "size z : " << (int)info[2] << std::endl;
+
+
         if (c.disconnectDevice()) {
             std::cout << "Disconnection from device successful" << std::endl;
             std::cout << "TEST PASSED " << std::endl;
