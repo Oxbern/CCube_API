5567f0568a3fa6d250d9810d4998ead78588f9a6
Yann Lachever <lachevery@gmail.com>
2016-06-09 14:00:28 +0200

    buffToString_test normalized

--- test/UnitTests/buffToString_test.cpp
+++ test/UnitTests/buffToString_test.cpp
@@ -1,6 +1,9 @@
 #include "Buffer.h"
 #include "Utils.h"
-#include "iostream"
+#include <iostream>
+#include <assert.h>
+
+#define DEBUG 1
 
 int main(int argc, char *argv[])
 {
@@ -19,14 +22,20 @@ int main(int argc, char *argv[])
     for (int i = 0; i < (SIZE_ACK - SIZE_CRC - DATA_INDEX); i++)
         ackBuff.setData(i, 0xFF);
 
+#if DEBUG
     std::cout << "Test buff toString Method" << std::endl;
     std::cout << "Expected : " << 1 << 1 << OPCODE(ACK_OK) << 0 << sizeAckData
               << 0XFF << 0xFF << 0xFF << 0 << 0 << std::endl;
-
     std::cout << "Result   : " << ackBuff.toString() << std::endl;
+#endif
 
-    std::cout <<"PASSED\n";
+    std::string resExpected = "11" + std::to_string(OPCODE(ACK_OK))
+                              + "0" + std::to_string(sizeAckData)
+                              + "255" + "255" + "255" + "0" + "0";
 
+    assert(ackBuff.toString() == resExpected);
+
+    std::cout <<"Test PASSED\n";
 
     return EXIT_SUCCESS;
 }
